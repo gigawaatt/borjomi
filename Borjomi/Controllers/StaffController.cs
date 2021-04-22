190a4fa36ed18102b57b1b0be6198ec9bbda49a8
Романов Сергей <cesarevih97@gmail.com>
2021-04-22 15:19:44 +0300

    adding files

---
+++ Borjomi/Controllers/StaffController.cs
@@ -0,0 +1,33 @@
+﻿using Borjomi.Data.Intetfaces;
+using Borjomi.Data.Models;
+using Borjomi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Borjomi.Controllers
+{
+    public class StaffController : Controller
+    {
+        private readonly IStaff _allStaff;
+
+        public StaffController(IStaff allStaff)
+        {
+            _allStaff = allStaff;
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Title = "Сотрудники";
+            StaffListViewModel obj = new StaffListViewModel();
+            obj.getAllStaff = _allStaff.AllStaff;
+            return View(obj);
+        }
+
+
+        
+
+    }
+}
---
+++ Borjomi/ViewModels/StaffListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Borjomi.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Borjomi.ViewModels
+{
+    public class StaffListViewModel
+    {
+        public IEnumerable<Staff> getAllStaff { get; set; }
+    }
+}
